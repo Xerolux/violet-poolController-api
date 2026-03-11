6c7fe6e56de5a83de72a8b6df733650006ba1b97
Basti <89860334+Xerolux@users.noreply.github.com>
2026-03-11 12:25:03 +0100

    Modify setup.py with new author email and license
    Updated author email and license information in setup.py.

--- setup.py
+++ setup.py
@@ -4,18 +4,26 @@ setup(
     name="violet-poolController-api",
     version="0.0.1",
     author="Basti (Xerolux)",
+    author_email="git@xerolux.de",
     description="Asynchronous Python client for the Violet Pool Controller.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Xerolux/violet-poolController-api",
     packages=find_packages(),
+    license="PolyForm Noncommercial License 1.0.0",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: Other/Proprietary License",
         "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Home Automation",
     ],
     python_requires=">=3.12",
     install_requires=[
         "aiohttp>=3.9.0",
     ],
+    project_urls={
+        "Bug Tracker": "https://github.com/Xerolux/violet-poolController-api/issues",
+        "License": "https://polyformproject.org/licenses/noncommercial/1.0.0/",
+    },
 )
